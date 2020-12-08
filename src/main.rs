d6b75e8d15ae16cb93cf96f1497a6c94a0142206
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-12-08 14:00:20 +0900

    table

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,30 @@ use todo_server::common::error::AlmaError;
 use todo_server::repository::template::{TodoEntry, IndexTemplate, render};
 
 use actix_web::{get, App, HttpResponse, HttpServer};
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
+
+    // db
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    ).expect("Failed to create a table `todo`");
+
+    // server
+    HttpServer::new(move || App::new().service(index).data(pool.clone()))
         .bind("127.0.0.1:7777")?
         .run()
         .await?;

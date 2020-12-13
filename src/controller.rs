cb8e703fcd0f545e5b866b260597901c11d393ee
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-12-13 13:32:50 +0900

    a

--- src/controller.rs
+++ src/controller.rs
@@ -1,9 +1,9 @@
 pub mod router {
 
     use crate::common::error::AlmaError;
-    use crate::repository::template::{TodoEntry, IndexTemplate, render, AddParams, DeleteParams};
+    use crate::repository::template::{render, AddParams, DeleteParams, IndexTemplate, TodoEntry};
 
-    use actix_web::{get, post, HttpResponse, web, http::header};
+    use actix_web::{get, http::header, post, web, HttpResponse};
     use r2d2::Pool;
     use r2d2_sqlite::SqliteConnectionManager;
     use rusqlite::params;
@@ -12,7 +12,6 @@ pub mod router {
     pub async fn index(
         db: web::Data<Pool<SqliteConnectionManager>>,
     ) -> Result<HttpResponse, AlmaError> {
-
         // db connect
         let conn = db.get()?;
 
@@ -38,12 +37,22 @@ pub mod router {
             .body(response_body))
     }
 
-
-
     #[post("/add")]
-    async fn add_todo(params: web::Form<AddParams>, db: web::Data<r2d2::Pool<SqliteConnectionManager>>) -> Result<HttpResponse, AlmaError> {
+    async fn add_todo(
+        params: web::Form<AddParams>,
+        db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+    ) -> Result<HttpResponse, AlmaError> {
         let conn = db.get()?;
         conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+        Ok(HttpResponse::SeeOther()
+            .header(header::LOCATION, "/")
+            .finish())
+    }
+
+    #[post("/delete")]
+    async fn delete_todo(params: web::Form<DeleteParams>, db: web::Data<r2d2::Pool<SqliteConnectionManager>>) -> Result<HttpResponse, AlmaError> {
+        let conn = db.get()?;
+        conn.execute("DELETE FROM todo WHERE id=?", &[params.id])?;
         Ok(HttpResponse::SeeOther().header(header::LOCATION, "/").finish())
     }
 }
